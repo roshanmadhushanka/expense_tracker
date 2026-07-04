b61d1b223960a9a3117120e5415311b359a00227
Roshan Alwis <13401713+roshanmadhushanka@users.noreply.github.com>
2026-07-04 09:48:58 -0500

    Add Expense model

--- lib/models/expense_model.dart
+++ lib/models/expense_model.dart
@@ -1,39 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Expense {
   final String id;
   final double amount;
-  final String description;
   final String category;
   final DateTime date;
-  final String userId;
+  final String description;
 
   Expense({
     required this.id,
     required this.amount,
-    required this.description,
     required this.category,
     required this.date,
-    required this.userId,
+    required this.description,
   });
 
-  Map<String, dynamic> toMap() {
+  factory Expense.fromFirestore(Map<String, dynamic> data) {
+    return Expense(
+      id: data['id'],
+      amount: data['amount'].toDouble(),
+      category: data['category'],
+      date: (data['date'] as Timestamp).toDate(),
+      description: data['description'],
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
     return {
       'id': id,
       'amount': amount,
-      'description': description,
       'category': category,
-      'date': date.toIso8601String(),
-      'userId': userId,
+      'date': date,
+      'description': description,
     };
-  }
-
-  factory Expense.fromMap(Map<String, dynamic> map, String id) {
-    return Expense(
-      id: id,
-      amount: map['amount']?.toDouble() ?? 0.0,
-      description: map['description'] ?? '',
-      category: map['category'] ?? '',
-      date: DateTime.parse(map['date']),
-      userId: map['userId'] ?? '',
-    );
   }
 }
